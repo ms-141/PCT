08bef49d9848ea1a4191cb34ced4fadcf2adeb0d
Muji <128879165+ms-141@users.noreply.github.com>
2026-03-30 12:14:13 -0600

    "Restructuring"

--- scraper.py
+++ scraper.py
@@ -1,25 +1,41 @@
 from bs4 import BeautifulSoup
 import requests
+from urllib.parse import urljoin
 
-# Need to loop over every course link and collect relevent iinfo.
+# Need to loop over every course link and collect relevent info
 
+#----------------------------------
+# SETUP 
 
+# main page for course catalog
 url = "https://catalog.mtroyal.ca/content.php?catoid=38&navoid=3076"
+
+# use the requests library to retrieve
 response  = requests.get(url)
 
-print(response)
-# print(response.text)
-
+# Use beautifulsoup to create an iterable object
 soup = BeautifulSoup(response.text, "html.parser")
 
-# only prints the first a tag element
-print(soup.a)
+course_links = []
 
+course_anchors = soup.select('a[href*="preview_course_nopop.php"]')
+for a in course_anchors:
+    href = a.get("href")
+    if href:
+        full_url = urljoin(url, href)
+        course_links.append(full_url)
 
-course_links = soup.select('a[href*="preview_course_nopop.php]')
-print(course_links)
+print("There are: ", len(course_links), " course links")
+print(course_links[:10])
 
 
 page_links = soup.select('a[href*="filter%5Bcpage%5D"]')
-print(page_links)
+# print(page_links)
+
+# save the list of links into an array
+
+
+
+
+# save the list of page links into an array
 
